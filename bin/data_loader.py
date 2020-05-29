3618b162647a61455b8c6818f1a37e2c6ebc699f
Adam Klie <aklie@ucsd.edu>
2020-05-29 12:03:56 -0700

    handling pickling in dataloader

--- bin/data_loader.py
+++ bin/data_loader.py
@@ -6,13 +6,17 @@ import numpy as np
 class SNPDataset(data.Dataset):
     """Characterizes a dataset for PyTorch."""
 
-    def __init__(self, geno_file, pheno_file):
+    def __init__(self, geno_file, pheno_file, pickle=False):
         """Initialization.
         Args:
             geno_file
             pheno_file
         """
-        self.genotypes = pd.read_csv(geno_file, index_col=0)
+        if pickle == False:
+            self.genotypes = pd.read_csv(geno_file, index_col=0)
+        else:
+            self.genotypes = pd.read_pickle(geno_file)
+        
         self.phenotypes = pd.read_csv(pheno_file, index_col=0)
         self.list_ids = self.phenotypes.index
 
@@ -29,10 +33,10 @@ class SNPDataset(data.Dataset):
         return X,y
 
 
-def get_loader(genotype_file, phenotype_file, batch_size, shuffle, num_workers):
+def get_loader(genotype_file, phenotype_file, batch_size, shuffle, num_workers, pickle=False):
     """Returns torch.utils.data.DataLoader for geno dataset."""
 
-    geno = SNPDataset(geno_file=genotype_file, pheno_file=phenotype_file)
+    geno = SNPDataset(geno_file=genotype_file, pheno_file=phenotype_file, pickle=pickle)
     params = {'batch_size': batch_size, 'shuffle': shuffle,'num_workers': num_workers}
     data_loader = torch.utils.data.DataLoader(dataset=geno,**params)
     return data_loader
